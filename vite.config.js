02add91b10796cef57dca5a646c5e699300d6f4b
Egregor2011 <egregor2011@gmail.com>
2023-08-27 18:11:16 +0100

    Fixing type module resolution

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  esbuild: {
+    loader: "tsx",
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/lib/show.tsx"),
       name: "Show",
+      formats: ["es", "umd"],
       fileName: (format) => `show.${format}.js`,
     },
     rollupOptions: {
@@ -18,5 +24,11 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      include: ["src/lib/show.tsx"],
+    }),
+  ],
 });
