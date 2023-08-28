786007f2cfcb9b6c4512efa2021c158cce8401fa
Egregor2011 <egregor2011@gmail.com>
2023-08-28 21:04:16 +0100

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import dts from "vite-plugin-dts";
-import * as packageJson from "./package.json";
 
 export default defineConfig({
   esbuild: {
@@ -18,7 +17,7 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)],
+      external: ["react", "react-dom"],
       output: {
         globals: {
           react: "React",
